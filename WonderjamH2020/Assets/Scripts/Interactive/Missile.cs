1616e8a3a264ac22e6ab646ba25bf59ff84f0870
Quentin <quentin2.durand@wanadoo.fr>
2020-02-29 09:10:05 -0500

    simple missile launcher interaction

--- WonderjamH2020/Assets/Scripts/Interactive/Missile.cs
+++ WonderjamH2020/Assets/Scripts/Interactive/Missile.cs
@@ -4,29 +4,34 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
+    [Tooltip("Time to reach target")]
     [SerializeField] private float flightDuration = 5f;
+    [Tooltip("How hight the missile will reach")]
     [SerializeField] private float height = 5f;
     [SerializeField] private int missileDamage = 5;
-    [SerializeField] private House opponentHouse;
+    [SerializeField] public House opponentHouse;
+    private bool launched = false;
 
-
-    private void Start()
-    {
-        LaunchMissile();
-    }
 
     private void Update()
     {
-        if(Vector2.Distance(transform.position,opponentHouse.transform.position) < 0.1f)
+        if(launched && Vector2.Distance(transform.position,opponentHouse.transform.position) < 0.1f)
         {
             Explode();
         }
     }
 
-
+    public void Initialize(House _opponentHouse, float _flightDuration,float _height,int _missileDamage)
+    {
+        opponentHouse = _opponentHouse;
+        flightDuration = _flightDuration;
+        height = _height;
+        missileDamage = _missileDamage;
+    }
 
     public void LaunchMissile()
     {
+        launched = true;
         Vector3[] bezier = {gameObject.transform.position, opponentHouse.transform.position + height * Vector3.up
                             ,gameObject.transform.position + height * Vector3.up, opponentHouse.transform.position};
         LeanTween.move(gameObject, bezier, flightDuration).setEaseInExpo();
---
+++ WonderjamH2020/Assets/Scripts/Interactive/MissileLauncher.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ChoicePopup;
+using System;
+
+public class MissileLauncher : ChoicesSenderBehaviour
+{
+    [SerializeField] private GameObject missilePrefab;
+    private Missile missile;
+    [SerializeField] private House opponentHouse;
+    [SerializeField] private float flightDuration = 5f;
+    [SerializeField] private float height = 5f;
+    [SerializeField] private int missileDamage = 5;
+
+
+    public void RechargeMissile()
+    {
+        if(missile == null)
+        {
+            missile = Instantiate(missilePrefab, transform).GetComponent<Missile>();
+            missile.transform.position = transform.position;
+            missile.Initialize(opponentHouse,flightDuration,height,missileDamage);
+        }
+    }
+    public void Fire()
+    {
+        if(missile != null)
+        {
+            missile.LaunchMissile();
+        }
+    }
+
+    private void Upgrade()
+    {
+        Debug.Log("Upgrade");
+    }
+
+    public override List<Choice> GetChoices()
+    {
+        return new List<Choice>() {
+                new Choice("Feu !", () => Fire(), () => true),
+                new Choice("Recharger", () => RechargeMissile(), () => true),
+                new Choice("Upgrade", () => Upgrade(), () => true)
+            };
+    }
+}
