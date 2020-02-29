ad46801f459bf5b1ecf9717ae2dd76e1a53d7a38
Maëva Trivino <maevatri@aol.com>
2020-02-29 11:39:38 -0500

    :construction: display mapping button on mainmenu

---
+++ WonderjamH2020/Assets/Scripts/MainMenu/ChangeStartText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Rewired;
+using TMPro;
+using UnityEngine;
+
+public class ChangeStartText : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshPro startGameText;
+
+    private Rewired.Player inputManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputManager = ReInput.players.GetPlayer(0);
+        Debug.Log(inputManager.controllers.maps.GetFirstButtonMapWithAction("Validate", true).elementIdentifierName);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(inputManager.GetButton("Validate").ToString());
+    }
+}
