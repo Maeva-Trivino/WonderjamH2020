d1fc896a28ff322d6c22f8ac8d0ce668c31340fc
Fabin <fab.mckay@gmail.com>
2020-02-29 12:32:26 -0500

    :construction: Basic UI and scripts missing return to menu

---
+++ WonderjamH2020/Assets/Scripts/UI/EndScreen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Rewired;
+using UnityEngine;
+using TMPro;
+
+public class EndScreen : MonoBehaviour
+{
+    public TextMeshProUGUI winnerMessage;
+
+    public GameObject container;
+
+    private Rewired.Player inputManager;
+
+    private bool waitingForReturnToMenu = false;
+
+    public void Start()
+    {
+        inputManager = ReInput.players.GetPlayer(0);
+        container.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (waitingForReturnToMenu && inputManager.GetButtonDown("Return_to_menu"))
+        {
+            Debug.Log("RETURN !!");
+        }
+    }
+
+
+    public void Show(string winner)
+    {
+        winnerMessage.SetText(string.Format("Congratulations ! {0} has won !", winner));
+        container.SetActive(true);
+        waitingForReturnToMenu = true;
+    }
+}
