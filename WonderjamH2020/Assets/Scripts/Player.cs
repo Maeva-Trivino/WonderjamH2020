dbd9a378cbef6a46bfee2c4fba34553ca4324c27
Quentin <quentin2.durand@wanadoo.fr>
2020-02-28 19:00:54 -0500

    simple interaction joueur-objet

--- WonderjamH2020/Assets/Scripts/Player.cs
+++ WonderjamH2020/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using Rewired;
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
@@ -8,6 +9,10 @@ public class Player : MonoBehaviour
     private float speed = 2;
     [SerializeField]
     private int playerID = 0;
+    [SerializeField]
+    private GameObject popUp;
+    private bool inMenu;
+    private bool inQTE;
 
     private Rewired.Player inputManager;
     private Vector2 input, direction = Vector2.down; // direction will be used for animations
@@ -19,10 +24,11 @@ public class Player : MonoBehaviour
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
+
     private void Update()
     {
         input = new Vector2(inputManager.GetAxis("Horizontal"), inputManager.GetAxis("Vertical"));
-        if (input.magnitude < .1f) input = Vector2.zero;
+        if (inQTE || inMenu || input.magnitude < .1f) input = Vector2.zero;
 
         if( input != Vector2.zero)
         {
@@ -33,5 +39,28 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         _rigidbody2D.MovePosition(_rigidbody2D.position + speed * input * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(DisplayPopUp());
+    }
+
+    private IEnumerator DisplayPopUp()
+    {
+        inMenu = true;
+        //popUp.SetActive(true); 
+        bool choiceMade = false;
+        while(!choiceMade)
+        {
+            float input = inputManager.GetAxis("Horizontal");
+            if (inputManager.GetButton("Validate"))
+            {
+                Debug.Log("Validé !");
+                choiceMade = true;
+            }
+            yield return true;
+        }
+        inMenu = false;
     }
 }
