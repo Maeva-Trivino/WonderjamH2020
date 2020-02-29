a0b9d56541cb623359f71f3bd4541d1a939e2533
Maëva Trivino <maevatri@aol.com>
2020-02-29 12:16:13 -0500

    :construction: add validate button on joystick in rewired

--- WonderjamH2020/Assets/Scripts/MainMenu/ChangeStartText.cs
+++ WonderjamH2020/Assets/Scripts/MainMenu/ChangeStartText.cs
@@ -29,6 +29,6 @@ public class ChangeStartText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(inputManager.GetButton("Validate").ToString());
+       
     }
 }
