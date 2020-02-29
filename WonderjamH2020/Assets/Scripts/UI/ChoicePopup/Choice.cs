9c4951386a1f14d31024ad3ebdcb94aad534b9ec
Yoann HAFFNER <yoyofrom68@gmail.com>
2020-02-28 19:27:08 -0500

    Methods unimplemented for for choice popup

---
+++ WonderjamH2020/Assets/Scripts/UI/ChoicePopup/Choice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UI.ChoicePopup
+{
+    public class Choice
+    {
+        private Func<bool> delegateEnability;
+        private Action delegateAction;
+        public bool IsEnabled => delegateEnability();
+        public string Description { get; private set; }
+
+        public Choice(string description, Action action, Func<bool> enability)
+        {
+            delegateAction = action;
+            delegateEnability = enability;
+            Description = description;
+        }
+
+        public void Invoke() => delegateAction();
+    }
+}
---
+++ WonderjamH2020/Assets/Scripts/UI/ChoicePopup/ChoicePopup.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.ChoicePopup
+{
+    public class ChoicePopup : MonoBehaviour
+    {
+        #region Variables
+        #region Editor
+        [SerializeField]
+        private GameObject choicePrefab;
+        #endregion
+
+        #region Private
+        #endregion 
+        #endregion
+
+        #region Methods
+        #region Unity
+        private void Update()
+        {
+            
+        }
+        #endregion
+
+        #region Public
+        public void SetChoices(List<Choice> choices)
+        {
+
+
+            for (int i = 0; i < choices.Count; i++)
+            {
+                Choice choice = (Choice)choices[i];
+                Button button = Instantiate(choicePrefab).GetComponent<Button>();
+                DisplayButton(button, i, choices.Count);
+            }
+        }
+
+        /// <summary>
+        /// Displays the popup at a specific world position
+        /// </summary>
+        /// <param name="position"></param>
+        public void Display(Transform position)
+        {
+
+        }
+
+        public void Hide()
+        {
+
+        }
+
+        public void GoLeft()
+        {
+
+        }
+
+        public void GoRight()
+        {
+
+        }
+
+        public void Validate()
+        {
+            
+        }
+        #endregion
+
+        #region Private
+        private void SetButtonEnabled(Button button, bool isEnabled)
+        {
+            // todo
+        }
+
+        private void DisplayButton(Button button, int i, int count)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #endregion
+    }
+}
