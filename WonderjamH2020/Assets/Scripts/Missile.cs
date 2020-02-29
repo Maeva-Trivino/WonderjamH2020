51f70f2374a8989c2f3cd2e151defa4b11124a5f
Fabin <fab.mckay@gmail.com>
2020-02-29 10:37:56 -0500

    :bento: Missiles Blueprints

--- WonderjamH2020/Assets/Scripts/Missile.cs
+++ WonderjamH2020/Assets/Scripts/Missile.cs
@@ -1,13 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Gameplay.Delivery;
 using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
+    [SerializeField] private string name = "default";
+    [SerializeField] private int price = 10;
     [SerializeField] private float flightDuration = 5f;
     [SerializeField] private float height = 5f;
     [SerializeField] private int missileDamage = 5;
     [SerializeField] private House opponentHouse;
+    [SerializeField] public int timeToDeliver;
+
+    public Missile() : base() { }
+
+    public Missile(MissileBlueprint blueprint)
+    {
+        this.name = blueprint.name;
+        this.price = blueprint.price;
+        this.flightDuration = blueprint.flightDuration;
+        this.height = blueprint.height;
+        this.missileDamage = blueprint.missileDamage;
+        this.timeToDeliver = blueprint.timeToDeliver;
+    }
 
 
     private void Start()
