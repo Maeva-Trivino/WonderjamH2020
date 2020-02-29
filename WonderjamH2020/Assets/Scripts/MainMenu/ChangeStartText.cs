430ef22f0fcd77eedf91d52c10e3c151e4d8998b
Maëva Trivino <maevatri@aol.com>
2020-02-29 12:57:01 -0500

    :construction: texts startGame and credits

--- WonderjamH2020/Assets/Scripts/MainMenu/ChangeStartText.cs
+++ WonderjamH2020/Assets/Scripts/MainMenu/ChangeStartText.cs
@@ -6,29 +6,42 @@ using UnityEngine;
 
 public class ChangeStartText : MonoBehaviour
 {
+    #region TextMeshes
     [SerializeField]
-    private TextMeshPro startGameText;
+    private TextMeshProUGUI startGameText;
 
+    [SerializeField]
+    private TextMeshProUGUI creditsText;
+    #endregion
+
+    //InputManager
     private Rewired.Player inputManager;
+
+    //Detect Joysticks
+    private Joystick joystick;
     // Start is called before the first frame update
     void Start()
     {
         inputManager = ReInput.players.GetPlayer(0);
-        Joystick joystick = ReInput.controllers.GetJoystick(0);
-        if (joystick == null)
-        {
-            Debug.Log(inputManager.controllers.maps.GetFirstButtonMapWithAction(ControllerType.Keyboard,"Validate", true).elementIdentifierName);
-        }
-        else
-        {
-            Debug.Log(inputManager.controllers.maps.GetFirstButtonMapWithAction(ControllerType.Joystick, "Validate", true).elementIdentifierName);
-        }
-
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        joystick = ReInput.controllers.GetJoystick(0);
+        ChangeTextsMenu(startGameText, "Validate", "Start the game");
+        ChangeTextsMenu(creditsText, "Credits", "Credits");
+    }
+
+    private void ChangeTextsMenu(TextMeshProUGUI textMeshToChange, string nameAction, string descriptionAction)
+    {
+        if (joystick == null)
+        {
+            textMeshToChange.text = inputManager.controllers.maps.GetFirstButtonMapWithAction(ControllerType.Keyboard, nameAction, true).elementIdentifierName + " : " + descriptionAction;
+        }
+        else
+        {
+            textMeshToChange.text = inputManager.controllers.maps.GetFirstButtonMapWithAction(ControllerType.Joystick, nameAction, true).elementIdentifierName + " : " + descriptionAction;
+        }
     }
 }
