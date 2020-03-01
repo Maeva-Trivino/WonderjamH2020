29c94d0bc8efb550e513d99b50828b97d7aca808
Maëva Trivino <maevatri@aol.com>
2020-02-29 21:51:29 -0500

    :sparkles: :lipstick: Timer start game

--- WonderjamH2020/Assets/Scripts/UI/Timer.cs
+++ WonderjamH2020/Assets/Scripts/UI/Timer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Rewired;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
@@ -8,14 +9,24 @@ public class Timer : MonoBehaviour
     public TMPro.TextMeshProUGUI timerText;
 
     [SerializeField]
+    private TMPro.TextMeshProUGUI timerStartGameText;
+
+    [SerializeField]
     float initialTime;
     float remainingTime;
-    bool timeIsTicking = true;
+    bool timeIsTicking = false;
+
+    public Rewired.Player inputManager1;
+    public Rewired.Player inputManager2;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        ResetTimer();
+        inputManager1 = ReInput.players.GetPlayer(0);
+        inputManager2 = ReInput.players.GetPlayer(1);
+
+        StartCoroutine(ResetTimer());
     }
 
     // Update is called once per frame
@@ -49,9 +60,31 @@ public class Timer : MonoBehaviour
         timeIsTicking = true;
     }
 
-    public void ResetTimer()
+    public IEnumerator ResetTimer()
     {
         remainingTime = initialTime;
+        string secondes = ((int)remainingTime % 60).ToString();
+        timerText.text = ((int)remainingTime / 60).ToString() + ":" + secondes;
+
+        inputManager1.controllers.maps.SetAllMapsEnabled(false);
+        inputManager2.controllers.maps.SetAllMapsEnabled(false);
+
+        int compteur = 3;
+
+        while(compteur != 1)
+        {
+            yield return new WaitForSeconds(1.0f);
+            compteur--;
+            timerStartGameText.text = compteur.ToString();
+        }
+
+        yield return new WaitForSeconds(1.0f);
+        timerStartGameText.text = "GO !";
+        yield return new WaitForSeconds(1.0f);
+        timerStartGameText.text = null;
+
+        inputManager1.controllers.maps.SetAllMapsEnabled(true);
+        inputManager2.controllers.maps.SetAllMapsEnabled(true);
         timeIsTicking = true;
     }
 }
