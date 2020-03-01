0a5f3f78868bb7c1410782ba80e7672e07076b2e
Maëva Trivino <maevatri@aol.com>
2020-03-01 11:42:12 -0500

    :construction: :lipstick: début dialogue mémés

---
+++ WonderjamH2020/Assets/Scripts/UI/DialogueSceneScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Popup;
+using UnityEngine;
+
+public class DialogueSceneScript : MonoBehaviour
+{
+    #region sprites mémés
+    [SerializeField]
+    private Sprite memeNormale;
+
+    [SerializeField]
+    private Sprite memeSemiEnervee;
+
+    [SerializeField]
+    private Sprite memeEnervee;
+    #endregion
+
+    [SerializeField]
+    private DialoguePopup DialoguePopup;
+
+    [SerializeField]
+    private bool isMemeBlue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //DialoguePopup.Hide();
+        //StartCoroutine(RoutineSpeak());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //private IEnumerator RoutineSpeak()
+    //{
+    //    if (isMemeBlue)
+    //    {
+    //        //DialoguePopup.enabled = true;
+    //        //Speak("Your dog has pee in my flower again !");
+    //        //yield return new WaitForSeconds(1.0f);
+
+    //    }
+    //    else
+    //    {
+    //        //yield return new WaitForSeconds(2.0f);
+            
+    //        //Speak("He just make it more beautiful !");
+    //        //DialoguePopup.enabled = true;
+    //        //yield return new WaitForSeconds(1.0f);
+
+
+    //    }
+    //}
+
+    public void Speak(string message, float delay = 0)
+    {
+        float baseTime = 1.5f;
+        DialoguePopup.Display();
+        DialoguePopup.SetText(message);
+        if (delay == -1)
+        {
+            delay += baseTime;
+        }
+        StartCoroutine(DialoguePopup.PopupDeactivation(baseTime + delay));
+    }
+}
